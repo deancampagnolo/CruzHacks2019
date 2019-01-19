fff4d96f300643f43099f413cc922ca1a6d31c47
Dean Campagnolo <dcampagn@ucsc.edu>
2019-01-19 00:33:41 -0800

    All Basic Buttons Now Work

--- app/src/main/java/com/deancampagnolo/cruzhacks2019/MainActivity.java
+++ app/src/main/java/com/deancampagnolo/cruzhacks2019/MainActivity.java
@@ -1,7 +1,9 @@
 package com.deancampagnolo.cruzhacks2019;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClicked(View v){
+        switch(v.getId()){
+            case R.id.MainSubmitButton:
+                startActivity(new Intent(this, GetCode.class));
+                break;
+
+            case R.id.MainLocateButton:
+                startActivity(new Intent(this, LocationPicker.class));
+                break;
+        }
+
     }
 }
