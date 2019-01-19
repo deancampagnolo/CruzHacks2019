81f4e7d12a889792ca68395b27989fc3bea73687
Dean Campagnolo <dcampagn@ucsc.edu>
2019-01-19 03:57:42 -0800

    Locational Services Now Work

--- app/src/main/java/com/deancampagnolo/cruzhacks2019/FindLocation.java
+++ app/src/main/java/com/deancampagnolo/cruzhacks2019/FindLocation.java
@@ -1,16 +1,35 @@
 package com.deancampagnolo.cruzhacks2019;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class FindLocation extends AppCompatActivity {
 
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
     private double latitude;
     private double longitude;
+
+    private double userLatitude;
+    private double userLongitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +40,65 @@ public class FindLocation extends AppCompatActivity {
         latitude = lastIntent.getDoubleExtra("latitude", 0);//goes to 0 if doesn't get value
         longitude = lastIntent.getDoubleExtra("longitude", 0);//goes to 0 if doesn't get value
 
+        locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
 
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                //Log.d("Location", Double.toString(location.getLatitude()));
+                //Log.d("Location", "HHHHHHHHHHHHH");
+                userLatitude = location.getLatitude();
+                userLongitude = location.getLongitude();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+
+
+        }else{
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED){
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            }
+        }
     }
 
     public void onButtonClicked(View v){
@@ -32,13 +109,17 @@ public class FindLocation extends AppCompatActivity {
                 break;
 
             case R.id.MapButton:
+
                 //Friendly reminder that this is FindLocation and this will go to Map
                 Intent i = new Intent(this, MapsActivity.class);
                 i.putExtra("latitude", latitude );
                 i.putExtra("longitude", longitude);
+                i.putExtra("userLatitude", userLatitude);
+                i.putExtra("userLongitude",userLongitude);
                 startActivity(i);
 
                 break;
         }
     }
+
 }
--- app/src/main/java/com/deancampagnolo/cruzhacks2019/MapsActivity.java
+++ app/src/main/java/com/deancampagnolo/cruzhacks2019/MapsActivity.java
@@ -1,6 +1,8 @@
 package com.deancampagnolo.cruzhacks2019;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -8,14 +10,19 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private double latitude;
     private double longitude;
+    private double userLatitude;
+    private double userLongitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,28 +36,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Intent lastIntent = getIntent();
         latitude = lastIntent.getDoubleExtra("latitude", 0);//goes to 0 if doesn't get value
         longitude = lastIntent.getDoubleExtra("longitude", 0);//goes to 0 if doesn't get value
-
+        userLatitude = lastIntent.getDoubleExtra("userLatitude", 0);
+        userLongitude = lastIntent.getDoubleExtra("userLongitude", 0);
 
 
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
+
         LatLng location = new LatLng(latitude, longitude);
-        mMap.addMarker(new MarkerOptions().position(location).title("Where it is"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+        LatLng userLocation = new LatLng(userLatitude,userLongitude);
+        CircleOptions locationCircleOptions = new CircleOptions();
+        locationCircleOptions.center(location);
+        locationCircleOptions.radius(30);
+        locationCircleOptions.strokeColor(Color.RED);
+        locationCircleOptions.fillColor(Color.LTGRAY);
+        mMap.addCircle(locationCircleOptions);
+
+
+        mMap.addMarker(new MarkerOptions().position(userLocation).title("Where User is"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
+        //mMap.setMinZoomPreference(14);
     }
+
+
 }
